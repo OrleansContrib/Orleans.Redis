8c9219ad44218ad17673490a3da4a09386118da5
Alex McCool <Alex.McCool@ametek.com>
2020-01-07 15:25:59 -0800

    adding redis-inside

--- Tests/Orleans.Persistence.Redis.Tests/ClusterFixture.cs
+++ Tests/Orleans.Persistence.Redis.Tests/ClusterFixture.cs
@@ -2,8 +2,10 @@
 using Microsoft.Extensions.DependencyInjection;
 using Orleans.Hosting;
 using Orleans.TestingHost;
+using RedisInside;
 using StackExchange.Redis;
 using System;
+using System.Collections.Generic;
 
 namespace Orleans.Persistence.Redis.Tests
 {
@@ -13,23 +15,36 @@ namespace Orleans.Persistence.Redis.Tests
         public readonly IClusterClient Client;
         
         public readonly IDatabase Database;
+        private readonly RedisInside.Redis _redis;
 
         public ClusterFixture()
         {
-            Console.WriteLine("Initializing Orleans TestCluster");
+            _redis = new RedisInside.Redis();
+            Console.WriteLine(_redis.Endpoint.ToString());
 
+            Console.WriteLine("Initializing Orleans TestCluster");
             var builder = new TestClusterBuilder(1);
             builder.Options.ServiceId = Guid.NewGuid();
             builder.Options.ClusterId = "TestCluster";
             builder.AddSiloBuilderConfigurator<SiloConfigurator>();
             builder.AddClientBuilderConfigurator<ClientConfigurator>();
+
+            //this is one of the only ways to be able to pass data (the redis connection string) to the silo(s) that TestCluster will startup
+            builder.ConfigureHostConfiguration(config =>
+            {
+                config.AddInMemoryCollection(new Dictionary<string, string>()
+                {
+                    { nameof(RedisInside.Redis), _redis.Endpoint.ToString() }
+                });
+            });
+
             Cluster = builder.Build();
 
             Cluster.Deploy();
             Cluster.InitializeClient();
             Client = Cluster.Client;
 
-            var redisOptions = ConfigurationOptions.Parse("localhost:6379");
+            var redisOptions = ConfigurationOptions.Parse(_redis.Endpoint.ToString());
             var connection = ConnectionMultiplexer.ConnectAsync(redisOptions).Result;
             Database = connection.GetDatabase();
             Console.WriteLine("Initialized Orleans TestCluster");
@@ -39,14 +54,27 @@ namespace Orleans.Persistence.Redis.Tests
         {
             public void Configure(ISiloHostBuilder builder)
             {
+                //get the redis connection string from the testcluster's config
+                var redisEP = builder.GetConfigurationValue(nameof(RedisInside.Redis));
+
                 builder.AddMemoryGrainStorageAsDefault();
                 builder.AddRedisGrainStorage("REDIS-JSON", optionsBuilder => optionsBuilder.Configure(options =>
                 {
                     options.UseJson = true;
+                    options.DataConnectionString = redisEP;
                 }));
-                builder.AddRedisGrainStorage("REDIS-BINARY");
+                builder.AddRedisGrainStorage("REDIS-BINARY", optionsBuilder => optionsBuilder.Configure(options =>
+                {
+                    options.UseJson = false;
+                    options.DataConnectionString = redisEP;
+                }));
 
-                builder.AddRedisGrainStorage("PubSubStore");
+                builder.AddRedisGrainStorage("PubSubStore", optionsBuilder => optionsBuilder.Configure(options =>
+                {
+                    options.UseJson = false;
+                    options.DataConnectionString = redisEP;
+                }));
+
                 builder.AddSimpleMessageStreamProvider("SMSProvider");
             }
         }
@@ -64,6 +92,8 @@ namespace Orleans.Persistence.Redis.Tests
             Database.ExecuteAsync("FLUSHALL").Wait();
             Client.Dispose();
             Cluster.StopAllSilos();
+
+            _redis?.Dispose();
         }
     }
 }
